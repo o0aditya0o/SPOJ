bdde688dc76861bbeaa62a572f4b309325c02068
o0aditya0o <aditya.mnnit15@gmail.com>
2016-01-20 02:11:10 +0530

    TRT.cpp

---
+++ TRT.cpp
@@ -0,0 +1,69 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+ * Created By : Aditya Agarwal
+ * IT, MNNIT-ALLAHABAD 
+ * aditya.mnnit15@gmail.com
+ _._._._._._._._._._._._._._._._._._._._._.*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MP make_pair
+#define pb push_back
+#define rep(i,n) for(int i=0;i<n;i++)
+#define REP(i,a,b) for(int i=a;i<=b;i++)
+#define PER(i,a,b) for(int i=b;i>=a;i--)
+#define X first
+#define Y second
+#define all(c) c.begin(),c.end() //eg sort(all(v));
+
+ typedef long long ll;
+ typedef vector<int> vi; 
+ typedef vector< vi > vvi; 
+ typedef pair< int,int > ii; 
+
+#define sz(a) int((a).size())
+#define clr(a) memset(a,0,sizeof(a))
+#define ini(a) memset(a,-1,sizeof(a))
+
+ //i/o
+#define inp(n) scanf("%d",&n)
+#define inpll(n) scanf("%lld",&n)
+#define inp2(n,m) inp(n), inp(m)
+#define ins(s) scanf("%s",s);
+#define out(n) printf("%d\n",n)
+#define outll(n) printf("%lld\n",n)
+#define out2(n,m) printf("%d %d\n",n,m)
+#define inc(n) scanf("%c",&n)
+
+
+//cost
+#define MOD 1000000007
+#define MOD_INV 1000000006
+#define MAX 100009
+#define INF 999999999
+#define mp make_pair
+typedef long long ll;
+typedef pair< pair<ll,ll>,ll > pairs;
+
+int n,dp[2001][2001],a[2001];
+
+ll mem(int low,int high,int age){
+	if(low==high)
+		return a[low]*(age);
+	if(dp[low][high]==0)
+	{
+		dp[low][high]=max(a[low]*(age)+mem(low+1,high,age+1),a[high]*(age)+mem(low,high-1,age+1));
+	}
+	return dp[low][high];
+}
+
+int main(){
+	inp(n);
+	rep(i,n)
+		inp(a[i]);
+	ll ans=mem(0,n-1,1);
+	cout<<ans<<endl;
+
+	return 0;
+}

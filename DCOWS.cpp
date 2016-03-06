728de7409d95a372022b4298af8f796560b639c2
Aditya Agarwal <aditya.mnnit15@gmail.com>
2016-03-06 20:14:38 +0530

    Update DCOWS.cpp

--- DCOWS.cpp
+++ DCOWS.cpp
@@ -21,7 +21,7 @@ int main(){
 	for(int i=0;i<=m;i++)
 		dp[0][i]=0;
 	for(int i=1;i<=n;i++)
-		dp[i][0]=9999999;
+		dp[i][0]=9999999999999;
 	for(int i=1;i<=n;i++){
 		for(int j=1;j<=m;j++){
 			dp[i][j]=min(dp[i][j-1],dp[i-1][j-1]+abs(bulls[i]-cows[j]));
